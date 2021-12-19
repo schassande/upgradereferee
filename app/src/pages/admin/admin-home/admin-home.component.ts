b01509742f25556254d377b4cd9d3f99942c113f
Sebastien Chassande <chassande@gmail.com>
2021-12-19 14:22:51 +0100

    permit to the admin to download users list

--- app/src/pages/admin/admin-home/admin-home.component.ts
+++ app/src/pages/admin/admin-home/admin-home.component.ts
@@ -2,6 +2,7 @@ import { NavController } from '@ionic/angular';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/UserService';
 import { DateService } from 'src/app/service/DateService';
+import { ConnectedUserService } from 'src/app/service/ConnectedUserService';
 
 @Component({
   selector: 'app-admin-home',
@@ -11,6 +12,7 @@ import { DateService } from 'src/app/service/DateService';
 export class AdminHomeComponent implements OnInit {
   downloading = false;
   constructor(
+    private connectedUserService: ConnectedUserService,
     private dateService: DateService,
     private navController: NavController,
     private userService: UserService
@@ -37,7 +39,53 @@ export class AdminHomeComponent implements OnInit {
       const url = URL.createObjectURL(oMyBlob);
       const a = document.createElement('a');
       a.href = url;
-      a.download = `Referee-Coaches_${this.dateService.date2string(new Date())}.csv`;
+      a.download = `Upgrade-Referee_Coaches_${this.dateService.date2string(new Date())}.csv`;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+      this.downloading = false;
+    });
+  }
+  exportRefereeList() {
+    this.downloading = true;
+    const SEP = ',';
+    this.userService.searchReferees(undefined).subscribe((rusers) => {
+      const content = `Id${SEP}First Name${SEP}Last Name${SEP}Email${SEP}Country${SEP}Referee Level${SEP}Referee Category${SEP}Referee Next Level\n` +
+        rusers.data
+          .filter(ref => {
+            return ref.accountStatus === 'ACTIVE'
+              && ref.applications.filter(app => app.name === 'Upgrade' && app.role === 'REFEREE').length > 0
+              && ref.referee && ref.referee.nextRefereeLevel;
+          }).map(user => user.id + SEP + user.firstName + SEP + user.lastName + SEP + user.email + SEP + user.country
+            + SEP + user.referee.refereeLevel + SEP + user.referee.refereeCategory + SEP + user.referee.nextRefereeLevel)
+          .join('\n');
+      const oMyBlob = new Blob([content], {type : 'text/csv'});
+      const url = URL.createObjectURL(oMyBlob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `Upgrade-Referees-_${this.dateService.date2string(new Date())}.csv`;
+      a.click();
+      window.URL.revokeObjectURL(url);
+      a.remove();
+      this.downloading = false;
+    });
+  }
+  exportNDRList() {
+    this.downloading = true;
+    const SEP = ',';
+    this.userService.searchUsers({ role: 'NDR', region: this.connectedUserService.getCurrentUser().region}).subscribe((rusers) => {
+      const content = `Id${SEP}First Name${SEP}Last Name${SEP}Email${SEP}Country\n` +
+        rusers.data
+          .filter(ref => {
+            return ref.accountStatus === 'ACTIVE'
+              && ref.applications.filter(app => app.name === 'Upgrade' && app.role === 'NDR').length > 0;
+          }).map(user => user.id + SEP + user.firstName + SEP + user.lastName + SEP + user.email + SEP + user.country)
+          .join('\n');
+      const oMyBlob = new Blob([content], {type : 'text/csv'});
+      const url = URL.createObjectURL(oMyBlob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = `Upgrade-NDRs-_${this.dateService.date2string(new Date())}.csv`;
       a.click();
       window.URL.revokeObjectURL(url);
       a.remove();
